b3b0b298568d2ca5ac68e298a31c060e0e4e7eee
André Mossige <mossige.a@gmail.com>
2021-01-28 13:22:59 +0100

    Legg kunInterne i URL

--- src/søk/Søk.tsx
+++ src/søk/Søk.tsx
@@ -1,13 +1,23 @@
 import { CheckboksPanel } from 'nav-frontend-skjema';
 import React, { ChangeEvent, FunctionComponent, useState } from 'react';
 import Søkefelt from './søkefelt/Søkefelt';
+import { byggUrlMedParam, hentSøkekriterier, QueryParam } from './søkefelt/urlUtils';
+import { useHistory, useLocation } from 'react-router-dom';
 import './Søk.less';
 
 const Søk: FunctionComponent = () => {
-    const [kunInterne, setKunInterne] = useState<boolean>(false); // TODO: Hent fra URL
+    const history = useHistory();
+    const location = useLocation();
+
+    const [kunInterne, setKunInterne] = useState<boolean>(
+        hentSøkekriterier(location.search).kunInterne
+    );
 
     const onKunInterneChange = (event: ChangeEvent<HTMLInputElement>) => {
-        setKunInterne(event.target.checked);
+        const kunInterne = event.target.checked;
+        setKunInterne(kunInterne);
+        const url = byggUrlMedParam(QueryParam.KunInterne, kunInterne);
+        history.replace({ search: url.search });
     };
 
     return (
