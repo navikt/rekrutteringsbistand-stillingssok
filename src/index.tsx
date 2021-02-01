cdea73b3a6efefa1b16518ec5af5e4817e6b42fb
Joar Aurdal <joar.aurdal@nav.no>
2021-02-01 09:39:43 +0100

    Legg til senty i appen

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,21 @@
 import React, { FunctionComponent } from 'react';
 import ReactDOM from 'react-dom';
 import Navspa from '@navikt/navspa';
+import * as Sentry from '@sentry/react';
 
 import App from './App';
 import Utviklingsapp from './utviklingsapp/Utviklingsapp';
+import { fjernPersonopplysninger, getMiljø } from './sentryUtils';
 import './index.less';
 import { Router } from 'react-router-dom';
+
+Sentry.init({
+    dsn: 'https://34e485d3fd9945e29d5f66f11a29f84e@sentry.gc.nav.no/43',
+    environment: getMiljø(),
+    release: process.env.REACT_APP_SENTRY_RELEASE || 'unknown',
+    enabled: getMiljø() === 'dev-fss' || getMiljø() === 'prod-fss',
+    beforeSend: fjernPersonopplysninger,
+});
 
 const skalEksporteres = process.env.REACT_APP_EXPORT || process.env.NODE_ENV === 'production';
 
@@ -15,9 +25,11 @@ export const cssScopeForApp = 'rekbis-stillingssok';
 
 const AppMedCssScope: FunctionComponent = (props: any) => (
     <div className={cssScopeForApp}>
-        <Router history={props.history}>
-            <App {...props} />
-        </Router>
+        <Sentry.ErrorBoundary>
+            <Router history={props.history}>
+                <App {...props} />
+            </Router>
+        </Sentry.ErrorBoundary>
     </div>
 );
 
