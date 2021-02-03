21f22977775250fa52e69b0129fb266c93fc9718
André Mossige <mossige.a@gmail.com>
2021-02-03 09:47:24 +0100

    Oppdater forklarende tekst

--- src/introduksjon/Introduksjon.tsx
+++ src/introduksjon/Introduksjon.tsx
@@ -10,16 +10,17 @@ const Introduksjon: FunctionComponent = () => {
                 <Forstørrelsesglass />
             </div>
             <div className="introduksjon__tekst">
-                <Undertittel className="blokk-xxs">Nytt stillingssøk</Undertittel>
+                <Undertittel className="blokk-xxs">Prøv det nye stillingssøket</Undertittel>
                 <Normaltekst className="blokk-xs">
-                    Første versjon av søket har få filtreringsmuligheter, men selve søket rommer
-                    flere søkeord. I tillegg til å søke på arbeidsgiver, annonsenummer og tittel,
-                    kan du nå søke etter ord i selve annonseteksten. Du kan kun søke i aktive
-                    stillinger.
+                    Vi har laget et nytt søk som gir flere muligheter for treff. Du kan nå søke
+                    etter ord i selve annonsen, i tillegg til å søke på arbeidsgiver, annonsenummer
+                    og tittel på stillingen. I den første versjonen får du færre muligheter til å
+                    filtrere søkeresultatet, og du kan bare søke på publiserte stillinger (ikke
+                    utløpte).
                 </Normaltekst>
                 <Normaltekst>
-                    Prøv det, og gi oss tilbakemelding ved å bruke tilbakemeldingsknappen nederst i
-                    hjørnet.
+                    Vil du prøve søket og gi oss en tilbakemelding? Bruk knappen for tilbakemelding
+                    nederst.
                 </Normaltekst>
             </div>
         </article>
