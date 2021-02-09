8ff4e1d720e753cbd990b8bda73976b0feb29abc
Joar Aurdal <joar.aurdal@nav.no>
2021-02-09 16:02:37 +0100

    Legg til inkludering subkategorier

--- src/søk/inkludering/Inkludering.tsx
+++ src/søk/inkludering/Inkludering.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, ChangeEvent } from 'react';
+import React, { FunctionComponent, ChangeEvent, Fragment } from 'react';
 import Ekspanderbartpanel from 'nav-frontend-ekspanderbartpanel';
 import { Checkbox } from 'nav-frontend-skjema';
 import { Enhetstype, hentEnhetstype } from '../../utils/skjermUtils';
@@ -6,6 +6,7 @@ import { hierarkiAvTagsForFilter } from './tags';
 
 const Inkludering: FunctionComponent = () => {
     const onHovedtagChange = (event: ChangeEvent<HTMLInputElement>) => {};
+    const onSubtagChange = (event: ChangeEvent<HTMLInputElement>) => {};
     const tagger = hierarkiAvTagsForFilter;
     console.log('tadaa', tagger);
     return (
@@ -16,15 +17,26 @@ const Inkludering: FunctionComponent = () => {
         >
             <>
                 {hierarkiAvTagsForFilter.map((t) => (
-                    <Checkbox
-                        className="søk__checkbox"
-                        label={t.hovedtag}
-                        value={t.hovedtag}
-                        checked={false}
-                        onChange={onHovedtagChange}
-                    />
+                    <Fragment key={t.hovedtag}>
+                        <Checkbox
+                            className="søk__checkbox"
+                            label={t.hovedtag.toLowerCase().replaceAll('_', ' ')}
+                            value={t.hovedtag}
+                            checked={false}
+                            onChange={onHovedtagChange}
+                        />
+                        {t.subtags.map((s) => (
+                            <Checkbox
+                                className="søk__checkbox søk__checkbox--indentert"
+                                key={s}
+                                label={s.split('__')[1].toLowerCase().replaceAll('_', ' ')}
+                                value={s}
+                                checked={false}
+                                onChange={onSubtagChange}
+                            />
+                        ))}
+                    </Fragment>
                 ))}
-                ,
             </>
         </Ekspanderbartpanel>
     );
