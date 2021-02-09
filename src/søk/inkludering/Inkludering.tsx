25e0344d2fe481239ca500ed73dbde0b5969714d
Joar Aurdal <joar.aurdal@nav.no>
2021-02-09 15:46:50 +0100

    Legg til tag hovedkategori checkboxer

--- src/søk/inkludering/Inkludering.tsx
+++ src/søk/inkludering/Inkludering.tsx
@@ -1,15 +1,31 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, ChangeEvent } from 'react';
 import Ekspanderbartpanel from 'nav-frontend-ekspanderbartpanel';
+import { Checkbox } from 'nav-frontend-skjema';
 import { Enhetstype, hentEnhetstype } from '../../utils/skjermUtils';
+import { hierarkiAvTagsForFilter } from './tags';
 
 const Inkludering: FunctionComponent = () => {
+    const onHovedtagChange = (event: ChangeEvent<HTMLInputElement>) => {};
+    const tagger = hierarkiAvTagsForFilter;
+    console.log('tadaa', tagger);
     return (
         <Ekspanderbartpanel
             apen={enhetstype === Enhetstype.Desktop}
             tittel="Inkludering"
             className="søk__ekspanderbart-panel"
         >
-            <div>Hei</div>
+            <>
+                {hierarkiAvTagsForFilter.map((t) => (
+                    <Checkbox
+                        className="søk__checkbox"
+                        label={t.hovedtag}
+                        value={t.hovedtag}
+                        checked={false}
+                        onChange={onHovedtagChange}
+                    />
+                ))}
+                ,
+            </>
         </Ekspanderbartpanel>
     );
 };
