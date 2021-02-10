1ee245eaf3a03a35df2b5b5d5b09583d6eed1f84
Joar Aurdal <joar.aurdal@nav.no>
2021-02-10 14:33:56 +0100

    Fjernet inkluderingskategorier

--- src/søk/inkludering/Inkludering.tsx
+++ src/søk/inkludering/Inkludering.tsx
@@ -2,11 +2,9 @@ import React, { FunctionComponent, ChangeEvent, Fragment } from 'react';
 import Ekspanderbartpanel from 'nav-frontend-ekspanderbartpanel';
 import { Checkbox } from 'nav-frontend-skjema';
 import { Enhetstype, hentEnhetstype } from '../../utils/skjermUtils';
-import { hierarkiAvTagsForFilter, visningsnavnForFilter } from './tags';
 
 const Inkludering: FunctionComponent = () => {
-    const onHovedtagChange = (event: ChangeEvent<HTMLInputElement>) => {};
-    const onSubtagChange = (event: ChangeEvent<HTMLInputElement>) => {};
+    const onInkluderingChange = (event: ChangeEvent<HTMLInputElement>) => {};
     return (
         <Ekspanderbartpanel
             apen={enhetstype === Enhetstype.Desktop}
@@ -14,27 +12,13 @@ const Inkludering: FunctionComponent = () => {
             className="søk__ekspanderbart-panel"
         >
             <>
-                {hierarkiAvTagsForFilter.map((gruppeMedTags) => (
-                    <Fragment key={gruppeMedTags.hovedtag}>
-                        <Checkbox
-                            className="søk__checkbox"
-                            label={visningsnavnForFilter[gruppeMedTags.hovedtag]}
-                            value={gruppeMedTags.hovedtag}
-                            checked={false}
-                            onChange={onHovedtagChange}
-                        />
-                        {gruppeMedTags.subtags.map((subtag) => (
-                            <Checkbox
-                                className="søk__checkbox søk__checkbox--indentert"
-                                key={subtag}
-                                label={visningsnavnForFilter[subtag]}
-                                value={subtag}
-                                checked={false}
-                                onChange={onSubtagChange}
-                            />
-                        ))}
-                    </Fragment>
-                ))}
+                <Checkbox
+                    className="søk__checkbox"
+                    label="Inkludering"
+                    value="INKLUDERING"
+                    checked={false}
+                    onChange={onInkluderingChange}
+                />
             </>
         </Ekspanderbartpanel>
     );
