cc21ffccb84a1124cb5274115eef8e1bd4da4bc4
André Mossige <mossige.a@gmail.com>
2021-03-02 15:17:19 +0100

    WIP: endre visning hvis favorittsøk er aktivt

--- src/søk/lagre-favorittsøk/LagreFavorittsøk.tsx
+++ src/søk/lagre-favorittsøk/LagreFavorittsøk.tsx
@@ -1,15 +1,38 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useEffect, useState } from 'react';
 import { Knapp } from 'nav-frontend-knapper';
 import { useLocation } from 'react-router-dom';
 
 const LagreFavorittsøk: FunctionComponent = () => {
     const { search } = useLocation();
+    const [favorittsøk, setFavorittsøk] = useState<string | null>(
+        localStorage.getItem(favorittsøkLocalstorageKey)
+    );
+    const [favorittsøkErAktivt, setFavorittsøkErAktivt] = useState<boolean>(favorittsøk === search);
+
+    useEffect(() => {
+        setFavorittsøkErAktivt(favorittsøk === search);
+    }, [search, favorittsøk]);
+
+    useEffect(() => {
+        const listener = () => {
+            setFavorittsøk(localStorage.getItem(favorittsøkLocalstorageKey));
+            console.log('setter favorittsøk', localStorage.getItem(favorittsøkLocalstorageKey));
+        };
+        window.addEventListener('storage', listener);
+        return () => {
+            window.removeEventListener('storage', listener);
+        };
+    });
 
     const onLagreSomFavorittsøkClick = () => {
         localStorage.setItem(favorittsøkLocalstorageKey, search);
     };
 
-    return <Knapp onClick={onLagreSomFavorittsøkClick}>Lagre som favorittsøk</Knapp>;
+    return favorittsøkErAktivt ? (
+        <div>bruker favorittsøk</div>
+    ) : (
+        <Knapp onClick={onLagreSomFavorittsøkClick}>Lagre som favorittsøk</Knapp>
+    );
 };
 
 export const favorittsøkLocalstorageKey = 'favorittsok';
