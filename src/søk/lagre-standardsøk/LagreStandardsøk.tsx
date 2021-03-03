763c0d5ff75b231dbb9f177c35432bbdef598df4
André Mossige <mossige.a@gmail.com>
2021-03-03 07:38:53 +0100

    Legg til hjelpetekst på standardsøk

--- src/søk/lagre-standardsøk/LagreStandardsøk.tsx
+++ src/søk/lagre-standardsøk/LagreStandardsøk.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponent, useEffect, useState } from 'react';
 import { Knapp } from 'nav-frontend-knapper';
 import { useLocation } from 'react-router-dom';
 import { AlertStripeSuksess } from 'nav-frontend-alertstriper';
+import Hjelpetekst from 'nav-frontend-hjelpetekst';
 
 const LagreStandardsøk: FunctionComponent = () => {
     const { search } = useLocation();
@@ -19,7 +20,13 @@ const LagreStandardsøk: FunctionComponent = () => {
     };
 
     return standardsøkErAktivt ? (
-        <AlertStripeSuksess>Lagret som standardsøk</AlertStripeSuksess>
+        <AlertStripeSuksess>
+            Lagret som standardsøk
+            <Hjelpetekst>
+                Hver gang du benytter søket vil det være ferdig utfylt med kriteriene du har valgt å
+                lagre som standardsøk.
+            </Hjelpetekst>
+        </AlertStripeSuksess>
     ) : (
         <Knapp onClick={onLagreSomStandardsøkClick}>Lagre som standardsøk</Knapp>
     );
