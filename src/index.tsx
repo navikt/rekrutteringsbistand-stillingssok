96943bc28ff99a660cbffa9b2b16d332a5499763
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2021-04-13 10:38:24 +0200

    La Sentry kun rapportere om egen app

--- src/index.tsx
+++ src/index.tsx
@@ -15,6 +15,7 @@ Sentry.init({
     release: process.env.REACT_APP_SENTRY_RELEASE || 'unknown',
     enabled: getMiljø() === 'dev-fss' || getMiljø() === 'prod-fss',
     beforeSend: fjernPersonopplysninger,
+    allowUrls: ['/rekrutteringsbistand-stillingssok'],
 });
 
 const skalEksporteres = process.env.REACT_APP_EXPORT || process.env.NODE_ENV === 'production';
