b834590e9373ff99bd9f1b3e9001bfe370c84d0d
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2022-02-11 08:19:52 +0100

    Enable AzureAD sidecar

--- src/api/api.ts
+++ src/api/api.ts
@@ -60,10 +60,10 @@ const logErrorResponse = (respons: Response) => {
     return `Statuskode: ${respons.status}, Statustekst: ${respons.statusText}, URL: ${respons.url}`;
 };
 
-const post = (url: string, body: object) => jsonRequestUtenCORS(url, body, 'POST');
-const put = (url: string, body: object) => jsonRequestUtenCORS(url, body, 'PUT');
+const post = (url: string, body: object) => jsonRequest(url, body, 'POST');
+const put = (url: string, body: object) => jsonRequest(url, body, 'PUT');
 
-const jsonRequestUtenCORS = (url: string, body: object, method: string) =>
+const jsonRequest = (url: string, body: object, method: string) =>
     fetch(url, {
         body: JSON.stringify(body),
         method,
