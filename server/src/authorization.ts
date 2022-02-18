edc1376d0a8fbee6e7ecad3f5e62502dfeb538c6
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2022-02-18 12:28:46 +0100

    Cache tokens

--- server/src/authorization.ts
+++ server/src/authorization.ts
@@ -29,6 +29,71 @@ export const removeIssoIdToken: Middleware = async (req, _, next) => {
     next();
 };
 
+type CachetOboToken = {
+    token: OboToken;
+    expires: number;
+};
+
+type NavIdent = string;
+
+const oboTokenCache: Record<NavIdent, CachetOboToken> = {};
+
+const tokenErFremdelesGyldig = (token: CachetOboToken) => token.expires >= Date.now() - 5000;
+
+const hentNyttOboToken = async (accessToken: string, scope: string): Promise<OboToken> => {
+    console.log('Bruker accessToken til å hente OBO-token');
+
+    const url = process.env.AZURE_OPENID_CONFIG_TOKEN_ENDPOINT;
+    const formData = {
+        grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+        scope: scope,
+        client_id: process.env.AZURE_APP_CLIENT_ID,
+        client_secret: process.env.AZURE_APP_CLIENT_SECRET,
+        assertion: accessToken,
+        requested_token_use: 'on_behalf_of',
+    };
+
+    console.log('Henter OBO token med innstillinger:', formData);
+
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams(formData),
+    });
+
+    if (response.ok) {
+        const token = await response.json();
+
+        console.log('Fikk OBO-token:', token);
+        return token;
+    } else {
+        throw new Error('Klarte ikke å hente nytt OBO-token');
+    }
+};
+
+const hentOboToken = async (accessToken: string, scope: string) => {
+    const oboTokenFraCache = oboTokenCache[accessToken];
+
+    if (oboTokenFraCache && tokenErFremdelesGyldig(oboTokenFraCache)) {
+        console.log('Bruker token fra cache som utløper', new Date(oboTokenFraCache.expires));
+        return oboTokenFraCache.token;
+    } else {
+        const nyttOboToken = await hentNyttOboToken(accessToken, scope);
+        const expires = Date.now() + nyttOboToken.expires_in * 1000;
+
+        console.log('Fikk nytt OBO-token som utløper', expires);
+
+        oboTokenCache[accessToken] = {
+            token: nyttOboToken,
+            expires,
+        };
+
+        return nyttOboToken;
+    }
+};
+
 export const setOnBehalfOfToken =
     (scope: string) => async (req: Request, res: Response, next: NextFunction) => {
         const accessToken = retrieveToken(req.headers);
@@ -36,36 +101,12 @@ export const setOnBehalfOfToken =
         if (!accessToken) {
             res.status(500).send('Kan ikke be om OBO-token siden accessToken ikke finnes');
         } else {
-            console.log('Bruker accessToken til å hente OBO-token');
+            try {
+                const token = await hentOboToken(accessToken, scope);
 
-            const url = process.env.AZURE_OPENID_CONFIG_TOKEN_ENDPOINT;
-            const formData = {
-                grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer',
-                scope: scope,
-                client_id: process.env.AZURE_APP_CLIENT_ID,
-                client_secret: process.env.AZURE_APP_CLIENT_SECRET,
-                assertion: accessToken,
-                requested_token_use: 'on_behalf_of',
-            };
-
-            console.log('Henter OBO token med innstillinger:', formData);
-
-            const response = await fetch(url, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                },
-                body: new URLSearchParams(formData),
-            });
-
-            if (response.ok) {
-                const body = (await response.json()) as OboToken;
-
-                console.log('Fikk OBO-token:', body);
-                // Cache
-                req.headers.authorization = `Bearer ${body.access_token}`;
+                req.headers.authorization = `Bearer ${token.access_token}`;
                 next();
-            } else {
+            } catch (e) {
                 res.status(500).send('Feil ved henting av OBO-token');
             }
         }
@@ -73,7 +114,7 @@ export const setOnBehalfOfToken =
 
 type OboToken = {
     access_token: string;
-    expires_in: string;
-    ext_expires_in: string;
+    expires_in: number;
+    ext_expires_in: number;
     token_type: string;
 };
