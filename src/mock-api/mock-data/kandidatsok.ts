9bd771148ceccd516ed7e0b6af6f1b92a9f76f65
Joar Aurdal <joar.aurdal@nav.no>
2023-06-08 13:42:36 +0200

    Legg til fil som mangler

---
+++ src/mock-api/mock-data/kandidatsok.ts
@@ -0,0 +1,12 @@
+const kandidatsøk = {
+    hits: {
+        total: { value: 1, relation: 'eq' },
+        hits: [
+            {
+                _source: { etternavn: 'Klippfisk', fornavn: 'Uklar' },
+            },
+        ],
+    },
+};
+
+export default kandidatsøk;
