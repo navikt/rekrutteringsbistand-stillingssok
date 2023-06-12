9d862a7fe8260f4bd7511b893a5ac5b2eeae52a9
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2023-06-12 10:39:39 +0200

    Map nye til gamle fylker i en fil

---
+++ src/søk/geografi/regionsreformen.ts
@@ -0,0 +1,11 @@
+const regionsreformen = {
+    Trøndelag: ['Nord-Trøndelag', 'Sør-Trøndelag'],
+    'Vestland ': ['Hordaland', 'Sogn og Fjordane'],
+    Agder: ['Aust-Agder', 'Vest-Agder'],
+    'Vestfold og Telemark': ['Vestfold', 'Telemark'],
+    Innlandet: ['Oppland', 'Hedmark'],
+    Viken: ['Buskerud', 'Akershus', 'Østfold'],
+    'Troms og Finnmark': ['Troms', 'Finnmark'],
+};
+
+export default regionsreformen;
