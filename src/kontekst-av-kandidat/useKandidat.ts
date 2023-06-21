d8e46483fd7739e4a749d5d859fcf600e16e8759
Joar Aurdal <joar.aurdal@nav.no>
2023-06-21 12:57:12 +0200

    Legg til nytt banner

---
+++ src/kontekst-av-kandidat/useKandidat.ts
@@ -0,0 +1,152 @@
+import { useEffect, useState } from 'react';
+import { QueryParam } from '../utils/urlUtils';
+import { brukNyttFylkesnummer } from '../filter/geografi/regionsreformen';
+import useNavigering from '../useNavigering';
+import fylkerOgKommuner from '../filter/geografi/fylkerOgKommuner.json';
+import { Status } from '../filter/om-annonsen/Annonsestatus';
+import { Publisert } from '../filter/om-annonsen/HvorErAnnonsenPublisert';
+import { Stillingskategori } from '../filter/om-annonsen/VelgStillingskategori';
+
+export const kandidatProxyUrl = '/kandidatsok-proxy';
+
+export type EsRespons = {
+    hits: {
+        hits: Array<{
+            _source: Kandidatrespons;
+        }>;
+    };
+};
+
+type Geografijobbønske = {
+    geografiKode: string;
+    geografiKodeTekst: string;
+};
+
+type Yrkejobbønske = {
+    sokeTitler: string[];
+};
+
+export type Kandidatrespons = {
+    fornavn: string;
+    etternavn: string;
+    fodselsdato: string | null;
+    adresselinje1: string | null;
+    postnummer: string | null;
+    poststed: string | null;
+    epostadresse: string | null;
+    telefon: string | null;
+    veileder: string | null;
+    arenaKandidatnr: string | null;
+    geografiJobbonsker: Geografijobbønske[];
+    yrkeJobbonskerObj: Yrkejobbønske[];
+};
+
+const byggQuery = (fodselsnummer: string) => ({
+    query: {
+        term: {
+            fodselsnummer,
+        },
+    },
+    size: 1,
+    _source: [
+        'fornavn',
+        'etternavn',
+        'fodselsdato',
+        'adresselinje1',
+        'postnummer',
+        'poststed',
+        'epostadresse',
+        'telefon',
+        'veileder',
+        'arenaKandidatnr',
+        'geografiJobbonsker',
+        'yrkeJobbonskerObj',
+    ],
+});
+
+function hentFylkestekstFraGeografiKode(geografiKode: string) {
+    return fylkerOgKommuner.find((fylke) => {
+        const fylkesnummerFraKandidat = geografiKode.split('.')[0].substring(2);
+        return fylke.fylkesnummer === brukNyttFylkesnummer(fylkesnummerFraKandidat);
+    })?.fylkesnavn;
+}
+
+const hentFylkerFraJobbønsker = (geografijobbønsker: Geografijobbønske[]): string[] => {
+    return geografijobbønsker
+        .map((jobbønske) => jobbønske.geografiKode)
+        .map((geografiKode) => hentFylkestekstFraGeografiKode(geografiKode))
+        .filter((fylke) => fylke !== undefined) as string[];
+};
+
+const hentKommunerFraJobbønsker = (geografijobbønsker: Geografijobbønske[]): string[] => {
+    return geografijobbønsker
+        .filter((jobbønske) => jobbønske.geografiKode.includes('.'))
+        .map((jobbønske) => {
+            const kommunetekst = jobbønske.geografiKodeTekst;
+            const fylkestekst = hentFylkestekstFraGeografiKode(jobbønske.geografiKode);
+
+            return `${fylkestekst}.${kommunetekst}`;
+        });
+};
+
+const hentYrkerFraJobbønsker = (yrkesønsker: Yrkejobbønske[]): string[] => {
+    return [...new Set(yrkesønsker.flatMap((yrkesønske) => yrkesønske.sokeTitler))];
+};
+
+const useKandidat = (fnr: string) => {
+    const { searchParams, navigate } = useNavigering();
+
+    const [kandidat, setKandidat] = useState<Kandidatrespons>();
+    const [feilmelding, setFeilmelding] = useState<string | undefined>();
+
+    useEffect(() => {
+        const hentKandidat = async (fnr: string) => {
+            try {
+                const respons = await fetch(kandidatProxyUrl, {
+                    method: 'POST',
+                    body: JSON.stringify(byggQuery(fnr)),
+                    headers: { 'Content-Type': 'application/json' },
+                });
+
+                const esRespons = (await respons.json()) as EsRespons;
+                const kandidat = esRespons.hits.hits.at(0)?._source;
+
+                if (kandidat) {
+                    setKandidat(kandidat);
+
+                    const fylkerFraKandidat = hentFylkerFraJobbønsker(kandidat.geografiJobbonsker);
+                    const kommunerFraKandidat = hentKommunerFraJobbønsker(
+                        kandidat.geografiJobbonsker
+                    );
+                    const yrkesønskerFraKandidat = hentYrkerFraJobbønsker(
+                        kandidat.yrkeJobbonskerObj
+                    );
+
+                    const søk = new URLSearchParams();
+
+                    søk.set(QueryParam.Fylker, String(fylkerFraKandidat));
+                    søk.set(QueryParam.Kommuner, String(kommunerFraKandidat));
+                    søk.set(QueryParam.Statuser, Status.Publisert);
+                    søk.set(QueryParam.Publisert, Publisert.Intern);
+                    søk.set(QueryParam.Stillingskategorier, Stillingskategori.Stilling);
+                    søk.set(QueryParam.Tekst, String(yrkesønskerFraKandidat));
+
+                    navigate({ search: søk.toString() }, { replace: true });
+                } else {
+                    setFeilmelding('Fant ikke kandidat med fødselsnummer ' + fnr);
+                }
+            } catch (e) {
+                setFeilmelding('Klarte ikke å hente kandidat');
+            }
+        };
+
+        hentKandidat(fnr);
+    }, [fnr]);
+
+    return {
+        kandidat,
+        feilmelding,
+    };
+};
+
+export default useKandidat;
